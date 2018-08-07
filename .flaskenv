d62e988370cb6614c27f51894b8b46b538aa1037
Ihsara <longchau21@gmail.com>
2018-08-07 22:50:19 +0200

    Add error handler

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,7 @@
 FLASK_APP=leso.py
+FLASK_ENV=development
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=
+MAIL_USERNAME=lesotanman.quantri@gmail.com
+MAIL_PASSWORD=lesotanman
--- leso.py
+++ leso.py
@@ -1,1 +1,30 @@
-from app import app
+# -*- coding: utf-8 -*-
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+if User.query.all() == []:
+    chau = User(username="TLC", email="longchau21@gmail.com")
+    chau.set_password('leso')
+    db.session.add(chau)
+    db.session.commit()
+
+    trang = User(username="TTKT", email="testemail@gmail.com")
+    trang.set_password('leso')
+    db.session.add(trang)
+    db.session.commit()
+
+elif User.query.filter_by(username='TLC').first() is None:
+    chau = User(username="TLC", email="longchau21@gmail.com")
+    chau.set_password('leso')
+    db.session.add(chau)
+    db.session.commit()
+
+elif User.query.filter_by(username='TTKT').first() is None:
+    trang = User(username="TTKT", email="testemail@gmail.com")
+    trang.set_password('leso')
+    db.session.add(trang)
+    db.session.commit()
