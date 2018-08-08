ce6727321fca45192de99400a268629b051b4559
Ihsara <longchau21@gmail.com>
2018-08-08 02:49:15 +0200

    Fix database problem

--- app/models.py
+++ app/models.py
@@ -20,8 +20,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    #about_me = db.Column(db.String(140))
-    #last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -68,7 +69,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), )
     cat_id = db.Column(db.Integer, db.ForeignKey('catergory.id'))
 
     def __repr__(self):
